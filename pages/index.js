0357acdaf18d3d1a92d0ec8dc030652e5125c22d
Jefrey <jefrey.bulla@gmail.com>
2023-10-30 17:30:27 -0400

    add next js

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+global.performance = global.performance || {
+  now: () => new Date().getTime(),
+};
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Welcome. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
